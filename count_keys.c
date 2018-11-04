309c26a70b25c0c804b5eef43da39f6e77d6e0c0
LuisRizo <personal.luisrizo@gmail.com>
2018-11-04 10:09:34 -0500

    Update comment

--- count_keys.c
+++ count_keys.c
@@ -7,7 +7,7 @@
 int count_keys(tree_node_t *tree, key_t a, key_t b) {
   if (tree->right == NULL) { // Found a leaf
     if (tree->key >= a && tree->key <= b) // Is a <= leaf->key <= b?
-      return 1; // If true, count++;
+      return 1; // If true, add 1 to count
     return 0; // else, do not count this leaf.
   }
   return count_keys(tree->left, a, b) + count_keys(tree->right, a, b);
